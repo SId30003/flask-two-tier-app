e2170aeab8e16a3deb6f94268f6eb58bed5819a8
Siddhant Wadhai <143829232+SId30003@users.noreply.github.com>
2026-02-19 02:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask
-import mysql.connector
-import time
 
 app = Flask(__name__)
 
-def connect_db():
-    while True:
-        try:
-            conn = mysql.connector.connect(
-                host="db",
-                user="root",
-                password="root",
-                database="testdb"
-            )
-            return conn
-        except:
-            time.sleep(2)
-
 @app.route('/')
 def home():
-    conn = connect_db()
-    return "Flask + MySQL CI/CD is Working!"
+    return "Flask CI/CD is Working 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
